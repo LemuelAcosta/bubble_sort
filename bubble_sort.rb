7a300a0c43b50b03ba87bbe54df87fce1c5bff88
Lemuel Acosta <12345678+LemuelAcosta@users.noreply.github.com>
2025-09-02 07:12:55 -0400

    Final commit

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,18 +2,16 @@ def bubble_sort(unsorted)
   arrSize = unsorted.length
   puts unsorted
   counter = 0
-  for i in 1...arrSize do
-    puts "i = #{i}"
-    if unsorted[i] < unsorted[i - 1]
-      first = unsorted[i - 1]
-      unsorted[i - 1] = unsorted[i]
-      unsorted[i] = first
-      puts "enter if"
+  for j in 0...arrSize
+    for i in 1...arrSize do
+      puts "i = #{i}"
+      if unsorted[i] < unsorted[i - 1]
+        first = unsorted[i - 1]
+        unsorted[i - 1] = unsorted[i]
+        unsorted[i] = first
+        puts "enter if"
+      end
     end
-    puts "--------"
-    puts unsorted
-    arrSize = arrSize - 1
-    counter = counter + 1
   end
   puts "counter"
   puts counter
